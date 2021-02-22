6e9f143de9a440b9bc1d273ba4a004991a1ffff5
May27Oliver <oliver310338@hotmail.com>
2021-02-23 07:23:43 +0800

    useState、useEffect in typescript

--- src/robot-gallery/App.tsx
+++ src/robot-gallery/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';//沒有這行會錯
+import React,{useState,useEffect} from 'react';//沒有這行會錯
 import logo from './assets/images/logo.svg';
 import styles from './App.module.css';
 import robots from './mockdata/robots.json';
@@ -10,35 +10,29 @@ interface Props {}
 interface State {
   robotGallery: any[]
 }
-class App extends React.Component< Props, State> {
-  constructor(props){
-    super(props);
-    this.state = {
-      robotGallery:[]
-    }
-  }
-  componentDidMount(){
+const App:React.FC<Props> = (props) =>{
+  const [count,setCount] = useState<number>(0);
+  const [robotGallery,setRobotGallery] = useState<any>([]);
+  useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
-    .then((res)=>{this.setState({robotGallery:res})})
-  }
-
-  render(){
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>
-            Oliver機器人精品專賣店
-          </h1>
-        </div>
-        <ShoppingCart/>
-        <div className={styles.robotList}>
-          {this.state.robotGallery.map( item => <Robot key={item.id} id={item.id} name={item.name} email={item.email}/>)}
-        </div>
+    .then(data=>{setRobotGallery(data)})
+  },[]);
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>
+          Oliver機器人精品專賣店
+        </h1>
       </div>
-    );
-  }
+      <ShoppingCart/>
+      <div className={styles.robotList}>
+        {console.log('robotGallery',robotGallery)}
+        {robotGallery.map( item => <Robot key={item.id} id={item.id} name={item.name} email={item.email}/>)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/robot-gallery/components/ShoppingCart.tsx
+++ src/robot-gallery/components/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './ShoppingCart.module.css';
 import { FiShoppingCart } from "react-icons/fi";
 interface Props {//傳入狀態
@@ -9,35 +9,26 @@ interface State {//組件狀態
     isOpen:boolean
 }
 
-class ShoppingCart extends React.Component<Props,State> {
-    constructor(props:Props){
-        super(props);
-        this.state = {
-            isOpen:false,
-        }
+const ShoppingCart:React.FC = (props) => {
+    const [open,setOpen] = useState(false);
+    const handleShowList = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{//將下面onClick的event資料型態寫進這裡的e後面
+        setOpen(open?false:true);
     }
-    handleShowList = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{//將下面onClick的event資料型態寫進這裡的e後面
-        this.setState({
-            isOpen:this.state.isOpen?false:true,
-        })
-    }
-    render(){
-        return(
-            <div className={styles.cardContainer}>
-                <button className={styles.button} 
-                onClick={this.handleShowList}
-                >
-                    <FiShoppingCart/>
-                    <span>購物車 2（件）</span></button>
-                {this.state.isOpen?<div className={styles.cartDropDown}>
-                    <ul>
-                        <li>robot 1</li>
-                        <li>robot 2</li>
-                    </ul>
-                </div>:''}
-            </div>
-        )
-    }
+    return(
+        <div className={styles.cardContainer}>
+            <button className={styles.button} 
+            onClick={handleShowList}
+            >
+                <FiShoppingCart/>
+                <span>購物車 2（件）</span></button>
+            {open?<div className={styles.cartDropDown}>
+                <ul>
+                    <li>robot 1</li>
+                    <li>robot 2</li>
+                </ul>
+            </div>:''}
+        </div>
+    )
 }
 
 export default ShoppingCart;
